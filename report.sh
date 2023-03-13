99969c4e9a27094fddb7889f9cccb902b8a6b371
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2023-03-13 15:21:14 +0100

    Add run scripts

---
+++ report.sh
@@ -0,0 +1,70 @@
+if [ ! $# -eq 3 ]
+then
+    echo "Usage: $0 Vagrantfile"
+    exit 1
+else
+    OS=$1
+    FULL=$2
+    BUILD=$3
+fi
+
+TITLE=$(grep -i -A 1 "Details might be found in the build log:" $FULL.log | tail -n1  | sed 's/.*portage\/\(.*\)\/temp.*/\1/')
+# compress both logs with bz2
+bzip2 -z $FULL
+bzip2 -z $BUILD
+FULL=$FULL.bz2
+BUILD=$BUILD.bz2
+
+##################
+# Report the bug #
+##################
+# Create info log
+echo "System:"  >> info_$SUFFIX.log
+echo "$OS" > info_$SUFFIX.log
+echo "" >> info_$SUFFIX.log
+echo "Steps to reproduce the bug:" >> info_$SUFFIX.log
+echo "Run the bootstrap-prefix.sh" >> info_$SUFFIX.log
+
+# Search for existing bugs
+bugz search "$TITLE" -r alexander@neuwirth-informatik.de 1> bgo_$SUFFIX.out 2> bgo_$SUFFIX.err
+if grep -Fxq "$TITLE" bgo_$SUFFIX.out
+then
+    # get bug id
+    id=$(grep "$TITLE" bgo_$SUFFIX.out | tail -n1 | awk '{print $1}')
+    bugz get $id 1> bgo_$SUFFIX.out 2> bgo_$SUFFIX.err
+    if grep -Fxq "$OS" bgo_$SUFFIX.out
+    then
+        # already reportet for this OS
+        echo "Bug already reported for $OS"
+    else
+        # add message fails for this os as well
+        bugz modify --comment "fails for $OS as well" $id 1>bgo_$SUFFIX.out 2>bgo_$SUFFIX.err
+        # attach logs
+        bugz attach --content-type "text/plain" --description "" $id $FULL 1>bgo_$SUFFIX.out 2>bgo_$SUFFIX.err
+        bugz attach --content-type "text/plain" --description "" $id $BUILD 1>bgo_$SUFFIX.out 2>bgo_$SUFFIX.err
+    fi
+else
+    # Post the bug
+    bugz post \
+        --product "Gentoo/Alt"          \
+        -a prefix@gentoo.org \
+        --component "Prefix Support"    \
+        --version "unspecified"           \
+        --title "$TITLE: bootstrap-prefix.sh fails"          \
+        --op-sys "Linux"                  \
+        --platform "All"                  \
+        --priority "Normal"               \
+        --severity "Normal"            \
+        --alias ""                        \
+        --description-from "./info_$SUFFIX.log"   \
+        --batch                           \
+        --default-confirm n               \
+        --cc alexander@neuwirth-informatik.de \
+        1>bgo_$SUFFIX.out 2>bgo_$SUFFIX.err 
+
+    id=$(grep "Info: Bug .* submitted" bgo_$SUFFIX.out | sed 's/[^0-9]//g')
+    # Attach the logs
+    bugz attach --content-type "text/plain" --description "" $id $FULL 1>bgo_$SUFFIX.out 2>bgo_$SUFFIX.err 
+    bugz attach --content-type "text/plain" --description "" $id $BUILD 1>bgo_$SUFFIX.out 2>bgo_$SUFFIX.err 
+fi
+
---
+++ run.sh
@@ -0,0 +1,27 @@
+if [ ! $# -eq 1 ]
+then
+    echo "Usage: $0 Vagrantfile"
+    exit 1
+else
+    export VAGRANT_VAGRANTFILE=$1
+fi
+
+TIME="$(shell date +%Y%m%d%H%M%S)"
+OS="$(shell echo $VAGRANT_VAGRANTFILE | sed 's/.*\///')"
+SUFFIX="$OS_$TIME"
+
+# Start the VM
+vagrant up
+# Stop Vagrant on failures
+die () {
+    vagrant halt
+    exit 1
+}
+
+# Jump start prefix
+vagrant ssh -c 'wget https://gitweb.gentoo.org/repo/proj/prefix.git/plain/scripts/bootstrap-prefix.sh && chmod +x bootstrap-prefix.sh && ./bootstrap-prefix.sh $PWD/gentoo-prefix noninteractive' > full_$SUFFIX.log || die
+grep -i -A 1 "Details might be found in the build log:" full_$SUFFIX.log | tail -n1  | sed 's/.*portage\/\(.*\)\/temp.*/\1/' || die 
+vagrant ssh -c "cat $(grep -i -A 1 'Details might be found in the build log:' full_$SUFFIX.log | tail -n1 )" > build_$SUFFIX.log || die 
+vagrant halt
+
+./report.sh $OS full_$SUFFIX.log build_$SUFFIX.log 
