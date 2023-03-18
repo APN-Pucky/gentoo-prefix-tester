f39380a984237d4050bbc7fd7eac88bd6fc112e7
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2023-03-18 19:08:02 +0100

    Run locally too

--- run.sh
+++ run.sh
@@ -6,13 +6,26 @@ else
     VAGRANT_VAGRANTFILE=$1
     KEY=$2
     STABLE=$3
+
+    TIME="$(date +%Y%m%d%H%M%S)"
+    # if VAGRANT_FILE is "LOCAL" 
+    if [ "$VAGRANT_VAGRANTFILE" = "LOCAL" ]
+    then
+        # use the local system
+        VAGRANTCMD="eval"
+        OS="$(uname -a | sed 's/\//_/g' | sed 's/ /_/g' | sed 's/#/_/g')"
+        # incdude OS? -> nope does not change (often...)
+        mkdir -p /tmp/gentoo-prefix-$TIME
+        cd /tmp/gentoo-prefix-$TIME
+    else
+        VAGRANTCMD="vagrant ssh -c"
+        OS="$(grep "config.vm.box" $VAGRANT_VAGRANTFILE | sed 's/.*= \"\(.*\)\"/\1/' | sed 's/\//_/g' )"
+    fi
+    SUFFIX="${STABLE}_${OS}_${TIME}"
     # export it to be used by vagrant
     export VAGRANT_VAGRANTFILE=$1
 fi
 
-TIME="$(date +%Y%m%d%H%M%S)"
-OS="$(grep "config.vm.box" $VAGRANT_VAGRANTFILE | sed 's/.*= \"\(.*\)\"/\1/' | sed 's/\//_/g' )"
-SUFFIX="${STABLE}_${OS}_${TIME}"
 FAILED=0
 
 # Start the VM
@@ -27,22 +40,21 @@ fail () {
     echo "Failed to build prefix"
     FAILED=1
 }
-
 # Jump start prefix
-vagrant ssh -c 'wget https://gitweb.gentoo.org/repo/proj/prefix.git/plain/scripts/bootstrap-prefix.sh && chmod +x bootstrap-prefix.sh' > "full_${SUFFIX}.log" || die
+$VAGRANTCMD 'wget https://gitweb.gentoo.org/repo/proj/prefix.git/plain/scripts/bootstrap-prefix.sh && chmod +x bootstrap-prefix.sh' >> "full_${SUFFIX}.log" || die
 # if unstable, remove export STABLE_PREFIX="yes" for non interactive mode
 if [ "$STABLE" = "UNSTABLE" ]
 then
-    vagrant ssh -c "sed -i 's/export STABLE_PREFIX=.*//g' bootstrap-prefix.sh" >> "full_${SUFFIX}.log" || die
+    $VAGRANTCMD "sed -i 's/export STABLE_PREFIX=.*//g' bootstrap-prefix.sh" >> "full_${SUFFIX}.log" || die
 fi
-vagrant ssh -c './bootstrap-prefix.sh $PWD/gentoo-prefix noninteractive' >> "full_${SUFFIX}.log" || fail
+$VAGRANTCMD './bootstrap-prefix.sh $PWD/gentoo-prefix noninteractive' >> "full_${SUFFIX}.log" || fail
 
 # if failed, report
 if [ $FAILED -eq 1 ]
 then
     # Find out what failed
     grep -i -A 1 "Details might be found in the build log:" "full_${SUFFIX}.log" | tail -n1  | sed 's/.*portage\/\(.*\)\/temp.*/\1/' || die 
-    vagrant ssh -c "cat $(grep -i -A 1 'Details might be found in the build log:' "full_${SUFFIX}.log" | tail -n1 | sed 's/build\.log.*/build.log/' )" > "build_${SUFFIX}.log" || die 
+    $VAGRANTCMD "cat $(grep -i -A 1 'Details might be found in the build log:' "full_${SUFFIX}.log" | tail -n1 | sed 's/build\.log.*/build.log/' )" > "build_${SUFFIX}.log" || die 
 
     # Create info log
     echo "System:"  >> "info_${SUFFIX}.log"
