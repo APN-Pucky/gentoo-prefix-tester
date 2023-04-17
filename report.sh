fe1b4b2d07144efbf83442a1b10f65a844297520
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2023-04-17 10:03:53 +0200

    Use xz compression
    Closes: https://github.com/APN-Pucky/gentoo-prefix-tester/issues/8

--- report.sh
+++ report.sh
@@ -28,10 +28,11 @@ TITLE=$(grep -i -A 1 "Details might be found in the build log:" $FULL | tail -n1
 TITLE="$TITLE: bootstrap-prefix.sh fails"
 
 # compress both logs with bz2
-bzip2 -z $FULL
-bzip2 -z $BUILD
-FULL=$FULL.bz2
-BUILD=$BUILD.bz2
+# TODO do we want to keept the uncompressed logs with -k ?
+xz $FULL 
+xz $BUILD
+FULL=$FULL.xz
+BUILD=$BUILD.xz
 
 # with explicit connection name to avoid missing default config error
 BUGZ="bugz --key $KEY --config-file gentoo.conf --connection Gentoo"
@@ -74,8 +75,8 @@ if [ $NOBUG -ge 1 ] ; then
 
     id=$(grep "Info: Bug .* submitted" bgo.out | sed 's/[^0-9]//g')
     # Attach the logs
-    $BUGZ attach --content-type "application/x-bzip2" --description "" $id $FULL | tee bgo.out 
-    $BUGZ attach --content-type "application/x-bzip2" --description "" $id $BUILD | tee bgo.out 
+    $BUGZ attach --content-type "application/x-xz" --description "" $id $FULL | tee bgo.out 
+    $BUGZ attach --content-type "application/x-xz" --description "" $id $BUILD | tee bgo.out 
 else
     echo "Bug exists"
     #exit 1
