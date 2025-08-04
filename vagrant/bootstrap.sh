ec740ab8e28c41bc7d257ee91634abf3322f9bcc
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2025-08-04 12:19:34 +0200

    Install missing sudo

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,18 +2,30 @@
 # Identify if we need to run apt, dnf or yum
 if command -v dnf &> /dev/null; then
     echo "Using DNF package manager"
-    # install sudo
-    sudo dnf install -y sudo
+    if command -v sudo &> /dev/null; then
+        echo "Sudo is already installed"
+    else
+        # install sudo
+        dnf install -y sudo
+    fi
     source "$(dirname "$0")/bootstrap-dnf.sh"
 elif command -v apt-get &> /dev/null; then
     echo "Using APT package manager"
-    # install sudo
-    sudo apt-get install -y sudo
+    if command -v sudo &> /dev/null; then
+        echo "Sudo is already installed"
+    else
+        # install sudo
+        apt-get install -y sudo
+    fi
     source "$(dirname "$0")/bootstrap-apt.sh"
 elif command -v yum &> /dev/null; then
     echo "Using YUM package manager"
-    # install sudo
-    sudo yum install -y sudo
+    if command -v sudo &> /dev/null; then
+        echo "Sudo is already installed"
+    else
+        # install sudo
+        yum install -y sudo
+    fi
     source "$(dirname "$0")/bootstrap-yum.sh"
 else
     echo "No supported package manager found. Please install build tools manually."
